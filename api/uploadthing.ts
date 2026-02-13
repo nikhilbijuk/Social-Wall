71d814bcdd7a150315d185977cbb97600c1d6d18
NIKHIL BIJU <nikhilbijum@gmail.com>
2026-02-13 21:58:11 +0530

    fix: Switch UploadThing API to Edge Runtime to resolve 504 Gateway Timeout

--- api/uploadthing.ts
+++ api/uploadthing.ts
@@ -1,12 +1,8 @@
 import { createRouteHandler } from "uploadthing/server";
 import { appFileRouter } from "../src/server/uploadthing.js";
 
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-};
+export const runtime = "edge";
 
-export default createRouteHandler({
+export const { GET, POST } = createRouteHandler({
     router: appFileRouter,
 });
