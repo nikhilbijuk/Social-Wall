11bec2332c79d2138ec4faf3665f4ae9136d4e38
NIKHIL BIJU <nikhilbijum@gmail.com>
2026-02-14 21:14:59 +0530

    fix: switch to edge runtime for uploadthing v7

--- api/uploadthing.ts
+++ api/uploadthing.ts
@@ -1,6 +1,8 @@
 import { createRouteHandler } from "uploadthing/server";
 import { appFileRouter } from "../src/server/uploadthing.js";
 
+export const runtime = "edge";
+
 export default createRouteHandler({
     router: appFileRouter,
 });
