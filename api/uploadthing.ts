c98f576f2ac8bccf96a15ddff16a41abcc6560b4
NIKHIL BIJU <nikhilbijum@gmail.com>
2026-02-14 21:20:15 +0530

    fix: inline router for stability and increase build limits

--- api/uploadthing.ts
+++ api/uploadthing.ts
@@ -1,7 +1,22 @@
-import { createRouteHandler } from "uploadthing/server";
-import { appFileRouter } from "../src/server/uploadthing.js";
+import { createRouteHandler, createUploadthing, type FileRouter } from "uploadthing/server";
 
 export const runtime = "edge";
+
+const f = createUploadthing();
+
+const appFileRouter = {
+    imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+        .onUploadComplete(async ({ file }) => {
+            console.log("Upload complete:", file.url);
+            return { uploadedBy: "user", url: file.url };
+        }),
+
+    videoUploader: f({ video: { maxFileSize: "16MB", maxFileCount: 1 } })
+        .onUploadComplete(async ({ file }) => {
+            console.log("Video upload complete:", file.url);
+            return { uploadedBy: "user", url: file.url };
+        }),
+} satisfies FileRouter;
 
 export default createRouteHandler({
     router: appFileRouter,
