e9084a6c663eabec4d0b2e60f591ff2c2926ec99
NIKHIL BIJU <nikhilbijum@gmail.com>
2026-02-14 20:57:10 +0530

    fix: add mandatory .js extension for TS2835

--- api/uploadthing.ts
+++ api/uploadthing.ts
@@ -1,5 +1,5 @@
 import { createRouteHandler } from "uploadthing/server";
-import { appFileRouter } from "../src/server/uploadthing";
+import { appFileRouter } from "../src/server/uploadthing.js";
 
 export default createRouteHandler({
     router: appFileRouter,
