a902fda87ea19230e983f31ebfc1a1e3d053e4cf
NIKHIL BIJU <nikhilbijum@gmail.com>
2026-02-18 21:48:25 +0530

    Fix slug mismatch to mediaUploader and revert node version to 20.x

--- api/uploadthing.ts
+++ api/uploadthing.ts
@@ -3,9 +3,11 @@ import { createUploadthing, createRouteHandler } from "uploadthing/server";
 const f = createUploadthing();
 
 const fileRouter = {
-    imageUploader: f({
+    mediaUploader: f({
         image: { maxFileSize: "4MB", maxFileCount: 1 },
+        video: { maxFileSize: "16MB", maxFileCount: 1 },
     }).onUploadComplete(async ({ file }) => {
+        console.log("Media upload complete:", file.url);
         return { uploadedBy: "user", url: file.url };
     }),
 };
