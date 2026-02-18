b8d16bf8be88bf36ef5f925e809451eb64e02fc0
NIKHIL BIJU <nikhilbijum@gmail.com>
2026-02-18 21:23:02 +0530

    Restore UploadThing and set Node 18

--- api/uploadthing.ts
+++ api/uploadthing.ts
@@ -1,3 +1,15 @@
-export default function handler(req: any, res: any) {
-    res.status(200).json({ message: "API working" });
-}
+import { createUploadthing, createRouteHandler } from "uploadthing/server";
+
+const f = createUploadthing();
+
+const fileRouter = {
+    imageUploader: f({
+        image: { maxFileSize: "4MB", maxFileCount: 1 },
+    }).onUploadComplete(async ({ file }) => {
+        return { uploadedBy: "user", url: file.url };
+    }),
+};
+
+export default createRouteHandler({
+    router: fileRouter,
+});
