ef10ed94c1e5101d7f2ddfeb8039c0ce63a60de8
NIKHIL BIJU <nikhilbijum@gmail.com>
2026-02-18 20:16:22 +0530

    Diagnostic: apply simple API handler and restore vercel rewrites

--- api/uploadthing.ts
+++ api/uploadthing.ts
@@ -1,19 +1,3 @@
-import { createUploadthing, createRouteHandler } from "uploadthing/server";
-
-const f = createUploadthing();
-
-export const appFileRouter = {
-    mediaUploader: f({
-        image: { maxFileSize: "4MB", maxFileCount: 1 },
-        video: { maxFileSize: "16MB", maxFileCount: 1 },
-    }).onUploadComplete(async ({ file }) => {
-        console.log("Media upload complete:", file.url);
-        return { uploadedBy: "user", url: file.url };
-    }),
-};
-
-export type AppFileRouter = typeof appFileRouter;
-
-export default createRouteHandler({
-    router: appFileRouter,
-});
+export default function handler(req: any, res: any) {
+    res.status(200).json({ message: "API working" });
+}
