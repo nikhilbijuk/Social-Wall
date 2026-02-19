4519ad4696fd7a57a1b0628e618cd8ffe79e4c6f
NIKHIL BIJU <nikhilbijum@gmail.com>
2026-02-19 20:24:47 +0530

    Fix uploadthing runtime

--- api/uploadthing.ts
+++ api/uploadthing.ts
@@ -1,8 +1,13 @@
 import { createUploadthing, createRouteHandler } from "uploadthing/server";
+import type { FileRouter } from "uploadthing/server";
+
+export const config = {
+    runtime: "nodejs",
+};
 
 const f = createUploadthing();
 
-const fileRouter = {
+export const uploadRouter = {
     mediaUploader: f({
         image: { maxFileSize: "4MB", maxFileCount: 1 },
         video: { maxFileSize: "16MB", maxFileCount: 1 },
@@ -10,8 +15,8 @@ const fileRouter = {
         console.log("Media upload complete:", file.url);
         return { uploadedBy: "user", url: file.url };
     }),
-};
+} satisfies FileRouter;
 
 export default createRouteHandler({
-    router: fileRouter,
+    router: uploadRouter,
 });
